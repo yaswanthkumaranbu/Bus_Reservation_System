0dfa3c326769e928f58335adeed76082faf2d592
yaswanthkumaranbu <yaswanthkumaranbu@gmail.com>
2024-11-29 21:30:36 +0530

    Initiate for displaying bus interior

---
+++ src/com/bookbus/busreserve/model/BusUI.java
@@ -0,0 +1,21 @@
+package com.bookbus.busreserve.model;
+
+public class BusUI {
+	 private String busUi[][] = new String[][] {
+	        { "30", "29", "28", "27", "26" },
+	        { "25", "24", "23", "22", "21" },
+	        { "20", "19", "18", "17", "16" },
+	        { "15", "14", "13", "12", "11" },
+	        { "10", "9", "8", "7", "6" },
+	        { "5", "4", "3", "2", "1" },
+	        { "Driver", " ", " ", "Entry" }
+	    };
+
+	public String[][] getBusUi() {
+		return busUi;
+	}
+
+	public void setBusUi(String[][] busUi) {
+		this.busUi = busUi;
+	}
+}
